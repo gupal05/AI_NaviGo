4b7431ba6fa731da7b06b7958f3a6fe1258f884e
PlaidMoon99 <yebin.moon99@gmail.com>
2025-02-03 15:59:47 +0900

    practice

---
+++ src/main/java/com/nevigo/ai_navigo/controller/testController.java
@@ -0,0 +1,13 @@
+package com.nevigo.ai_navigo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class testController {
+
+    @GetMapping("/main")
+    public String mainPage() {
+        return "/main/home"; // home.html 파일과 매핑
+    }
+}
