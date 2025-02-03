e5cf672e712cd22a1b01dafc6903072e40c5f637
PlaidMoon99 <yebin.moon99@gmail.com>
2025-02-03 10:26:08 +0900

    master to main

---
+++ src/main/java/com/nevigo/ai_navigo/AiNaviGoApplication.java
@@ -0,0 +1,13 @@
+package com.nevigo.ai_navigo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiNaviGoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiNaviGoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nevigo/ai_navigo/AiNaviGoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nevigo.ai_navigo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiNaviGoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
