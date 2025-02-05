acbbf5cb8bd20bb407a771e5b69bcfe8159acd09
PlaidMoon99 <yebin.moon99@gmail.com>
2025-02-05 17:07:50 +0900

    delete error

--- src/main/java/com/nevigo/ai_navigo/controller/mypageController.java
+++ src/main/java/com/nevigo/ai_navigo/controller/mypageController.java
@@ -1,23 +1,18 @@
 package com.nevigo.ai_navigo.controller;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import java.security.Principal;
-
-import com.nevigo.ai_navigo.service.MemberUpdateService;
-
 @Controller
 @RequestMapping("/mypage")
-public class memberUpdateController {
+public class mypageController {
 
-    @GetMapping("/mypage")
+    @GetMapping
     public String myPage() {
-        return "/mypage/mypage";
+        return "mypage/mypage";
     }
 
     @GetMapping("/update")
     public String update() {
-        return "/mypage/updateForm";
+        return "mypage/updateForm";
     }
 }
