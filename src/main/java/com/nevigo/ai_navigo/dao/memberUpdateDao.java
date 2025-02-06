3f6db8c7b91a6d629996f06d238fc49a7df983a3
PlaidMoon99 <yebin.moon99@gmail.com>
2025-02-06 16:52:26 +0900

    mypage connect to db

--- src/main/java/com/nevigo/ai_navigo/dao/memberUpdateDao.java
+++ src/main/java/com/nevigo/ai_navigo/dao/memberUpdateDao.java
@@ -3,8 +3,10 @@ package com.nevigo.ai_navigo.dao;
 import com.nevigo.ai_navigo.dto.MemberDTO;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
 
 @Mapper
+@Repository
 public interface memberUpdateDao {
     MemberDTO findById(@Param("member_id") String memberId);  // 회원 조회
     void updateMember(MemberDTO member);  // 회원 정보 수정
