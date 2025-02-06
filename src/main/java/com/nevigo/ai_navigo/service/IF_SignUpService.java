48399a956b47ebf67c8d118b1db04c46be66f062
gupal05 <dudgh2349@naver.com>
2025-02-06 17:06:43 +0900

    clear signup

--- src/main/java/com/nevigo/ai_navigo/service/IF_SignUpService.java
+++ src/main/java/com/nevigo/ai_navigo/service/IF_SignUpService.java
@@ -2,7 +2,10 @@ package com.nevigo.ai_navigo.service;
 
 import com.nevigo.ai_navigo.dto.MemberDTO;
 
+import java.util.Map;
+
 public interface IF_SignUpService {
     public int dupCheckId(String dupCheckId);
     public int insMember(MemberDTO member);
+    public int insPreference(Map<String, Object> preference);
 }
