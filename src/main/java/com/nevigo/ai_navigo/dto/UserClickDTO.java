367073b2416f9e58f29ff8379649ccac09ac0327
kimsion <tkdlqhrm222@gmail.com>
2025-02-07 16:44:43 +0900

    userclickdto.java -20250207

---
+++ src/main/java/com/nevigo/ai_navigo/dto/UserClickDTO.java
@@ -0,0 +1,13 @@
+package com.nevigo.ai_navigo.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserClickDTO {
+    private String contentid;
+    private String cat1;
+    private String cat2;
+    private String cat3;
+}
