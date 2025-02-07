1937254f6dc9f1064c20f7c1470798c943bfd007
PlaidMoon99 <yebin.moon99@gmail.com>
2025-02-07 16:29:45 +0900

    ui

--- src/main/java/com/nevigo/ai_navigo/controller/mypageController.java
+++ src/main/java/com/nevigo/ai_navigo/controller/mypageController.java
@@ -22,32 +22,26 @@ public class mypageController {
     @GetMapping
     public String mypage(Model model, HttpSession session) {
         String memberId = (String) session.getAttribute("memberId");
-        if(memberId == null) {
-            return "redirect:/login"; // 로그인 안 되어 있으면 로그인 페이지로 리다이렉트
-        }
 
         // memberService를 통해 findById 호출
         MemberDTO member = memberService.getMemberById(memberId);  // Service 사용
         model.addAttribute("member", member);
-        return "mypage";  // mypage.jsp로 이동
+        return "/mypage/mypage";  // mypage.jsp로 이동
     }
 
     @GetMapping("/update")
     public String update(@RequestParam("memberId") String memberId, Model model) {
         MemberDTO member = memberService.getMemberById(memberId);  // 서비스 호출
         model.addAttribute("member", member);
-        return "updateForm";  // updateForm.jsp로 이동
+        return "/updateForm";  // updateForm.jsp로 이동
     }
 
     @PostMapping("/update")
     public String updateMember(@ModelAttribute MemberDTO member, HttpSession session) {
         String memberId = (String) session.getAttribute("memberId");
-        if (memberId == null) {
-            return "redirect:/login";  // 로그인 안 되어 있으면 로그인 페이지로 리다이렉트
-        }
 
         //member.setMemberId(memberId);  // 세션에서 ID를 가져와 설정
         memberService.updateMember(member);  // 서비스 호출하여 회원 정보 업데이트
-        return "redirect:/mypage";  // 업데이트 후 마이페이지로 리다이렉트
+        return "redirect:/mypage/update";  // 업데이트 후 마이페이지로 리다이렉트
     }
 }
