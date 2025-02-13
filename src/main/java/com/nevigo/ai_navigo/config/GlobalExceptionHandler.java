d99c17fbe2424a74aeab8691d53d0b8a6a8a3e11
gupal05 <dudgh2349@naver.com>
2025-02-13 16:54:03 +0900

    create img handler

---
+++ src/main/java/com/nevigo/ai_navigo/config/GlobalExceptionHandler.java
@@ -0,0 +1,17 @@
+package com.nevigo.ai_navigo.config;
+
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MaxUploadSizeExceededException.class)
+    public ResponseEntity<String> handleMaxSizeException(MaxUploadSizeExceededException ex) {
+        return ResponseEntity.status(HttpStatus.PAYLOAD_TOO_LARGE).body("파일 크기가 너무 큽니다! (최대 10MB)");
+    }
+}
+
