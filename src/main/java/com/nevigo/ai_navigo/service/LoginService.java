0afe5d6c6232a62a56c3af244d152285dc710dfe
gupal05 <dudgh2349@naver.com>
2025-02-17 17:10:01 +0900

    add method

--- src/main/java/com/nevigo/ai_navigo/service/LoginService.java
+++ src/main/java/com/nevigo/ai_navigo/service/LoginService.java
@@ -5,13 +5,19 @@ import com.nevigo.ai_navigo.dao.IF_SignUpDao;
 import com.nevigo.ai_navigo.dto.MemberDTO;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-@RequiredArgsConstructor
 public class LoginService implements IF_LoginService{
     private final IF_LoginDao loginDao;
+    private final BCryptPasswordEncoder passwordEncoder;
+
+    public LoginService(IF_LoginDao loginDao, BCryptPasswordEncoder passwordEncoder) {
+        this.loginDao = loginDao;
+        this.passwordEncoder = passwordEncoder;
+    }
 
     @Override
     @Transactional
@@ -25,9 +31,9 @@ public class LoginService implements IF_LoginService{
 
     @Override
     @Transactional
-    public int isMemberPw(MemberDTO member) {
+    public String getMemberPw(MemberDTO member) {
         try {
-            return loginDao.isMemberPw(member);
+            return loginDao.getMemberPw(member);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
@@ -42,5 +48,13 @@ public class LoginService implements IF_LoginService{
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public boolean isPasswordMatch(MemberDTO member, String pw) {
+        // DB에서 암호화된 비밀번호 가져오기
+        String encodedPassword = pw;
+
+        // 비밀번호 비교 (입력값 vs 암호화된 값)
+        return passwordEncoder.matches(member.getMemberPw(), encodedPassword);
     }
 }
