87545bed7b81bd0e0b95847edc1a97da603c69e0
kimsion <tkdlqhrm222@gmail.com>
2025-02-17 16:40:03 +0900

    mod

--- src/main/java/com/nevigo/ai_navigo/dao/IF_preferenceDao.java
+++ src/main/java/com/nevigo/ai_navigo/dao/IF_preferenceDao.java
@@ -5,6 +5,9 @@ import lombok.ToString;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Map;
+
 @Mapper
 @Repository
 public interface IF_preferenceDao {
@@ -12,5 +15,6 @@ public interface IF_preferenceDao {
     public String getPreferenceById(String memberId);
     public void setUserClickInfo(UserClickDTO userclickdto);
     public String getPopularCat3();
+    public List<Map<String, Object>> getPopularTitle10();
 
 }
