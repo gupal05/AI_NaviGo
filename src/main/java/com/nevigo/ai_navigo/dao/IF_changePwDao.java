5c068c80b8b4003ecb7cfb2b71b65df5609ddfbd
PlaidMoon99 <yebin.moon99@gmail.com>
2025-02-20 14:45:05 +0900

    changePw db연결완료

--- src/main/java/com/nevigo/ai_navigo/dao/IF_changePwDao.java
+++ src/main/java/com/nevigo/ai_navigo/dao/IF_changePwDao.java
@@ -1,14 +1,12 @@
 package com.nevigo.ai_navigo.dao;
 
-import com.nevigo.ai_navigo.dto.MemberDTO;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Mapper
 @Repository
 public interface IF_changePwDao {
     // 비밀번호 업데이트
-    void updatePassword(@Param("userId") String userId, @Param("newPw") String newPw);
+    void updatePassword(@Param("memberId") String memberId, @Param("newPw") String newPw);
 }
